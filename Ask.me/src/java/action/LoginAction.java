a3f83b954770da81c269f1a883ff3fa62e062514
Kevin Tripp <ktripp04@gmail.com>
2016-12-05 10:58:32 -0500

    Updated login actuion class

--- Ask.me/src/java/action/LoginAction.java
+++ Ask.me/src/java/action/LoginAction.java
@@ -4,11 +4,58 @@
  * and open the template in the editor.
  */
 package action;
+import java.util.Date;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
 
 /**
  *
- * @author sumnerib
+ * @author trippkm
  */
-public class LoginAction {
+public class LoginAction extends ActionSupport 
+{
+    private String userId;
+    private String passwd;
     
+    public String execute() throws Exception 
+    {
+        if("admin".equals(userId) && "password".equals(passwd))
+        {
+            Map session = ActionContext.getContext().getSession();
+            session.put("logined", "true");
+            session.put("context", new Date());
+            return SUCCESS;
+        }
+        return ERROR;
+    }
+    
+    public String logout() throws Exception
+    {
+        Map session = ActionContext.getContext().getSession();
+        session.remove("logined");
+        session.remove("context");
+        return SUCCESS;
+    }
+    
+    public String getPasswd()
+    {
+        return passwd;
+    }
+    
+    public void setPsswd(String passwd)
+    {
+        this.passwd = passwd;
+    }
+    
+    public String getUserId()
+    {
+        return userId;
+    }
+    
+    public void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
 }
