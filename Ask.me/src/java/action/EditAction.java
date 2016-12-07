72a2f64dbe77d000d1f6bac1b11c164bda19d104
zometaax <>
2016-12-07 14:36:46 -0500

    Added EditAction class, modified edit.html (unfinished)

---
+++ Ask.me/src/java/action/EditAction.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+/**
+ *
+ * @author zometaax
+ */
+public class EditAction extends ActionSupport {
+    
+    private String newUsername;
+    private String newPassword;
+    private String newName;
+    private String newDOBDay;
+    private String newDOBMonth;
+    private String newDOBYear;
+    private String newGender;
+    
+    public void validate() {
+        
+        
+    }
+    
+    public String execute() {
+        
+        return SUCCESS;
+    }
+
+    public String getNewUsername() {
+        return newUsername;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public String getNewDOBDay() {
+        return newDOBDay;
+    }
+
+    public String getNewDOBMonth() {
+        return newDOBMonth;
+    }
+
+    public String getNewDOBYear() {
+        return newDOBYear;
+    }
+
+    public String getNewGender() {
+        return newGender;
+    }
+
+    public void setNewUsername(String newUsername) {
+        this.newUsername = newUsername;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+
+    public void setNewDOBDay(String newDOBDay) {
+        this.newDOBDay = newDOBDay;
+    }
+
+    public void setNewDOBMonth(String newDOBMonth) {
+        this.newDOBMonth = newDOBMonth;
+    }
+
+    public void setNewDOBYear(String newDOBYear) {
+        this.newDOBYear = newDOBYear;
+    }
+
+    public void setNewGender(String newGender) {
+        this.newGender = newGender;
+    }
+    
+}
